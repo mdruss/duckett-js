3fd7f9df6c9393c12de4aa053066b05835e6d85b
Matt Russell <mdruss@gmail.com>
2015-09-21 21:40:13 -0400

    Started Ch2 of Duckett JS & JQ

---
+++ c01/add-content.js
@@ -0,0 +1,19 @@
+/* This script displays a greeting to the user based upon the current time. 
+It is an example from a JavaScript & jQuery book */
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+// Display the appropriate greeting based on the current time
+if (hourNow > 18) {
+	greeting = 'Good evening!';
+} else if (hourNow > 12) {
+	greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+	greeting = 'Good morning!';
+} else {
+	greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
